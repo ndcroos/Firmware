cd26d129902bffbc8236eeec1e391024e415662f
Author Lorenz Meier, Dennis Mannhart <dennis@px4.io>
2017-05-16 14:42:54 +0200

    rtl: brake, pre-return and after-return

--- src/modules/mc_pos_control/mc_pos_control_main.cpp
+++ src/modules/mc_pos_control/mc_pos_control_main.cpp
@@ -814,6 +814,7 @@ MulticopterPositionControl::poll_subscriptions()
 			_pos_sp_triplet.current.valid = true;
 
 		} else if (PX4_ISFINITE(_pos_sp_triplet.current.alt) && _pos_sp_triplet.current.valid) {
+
 			_pos_sp_triplet.current.valid = true;
 
 		} else {
@@ -1036,7 +1037,7 @@ MulticopterPositionControl::get_cruising_speed_xy()
 	/*
 	 * in mission the user can choose cruising speed different to default
 	 */
-	return ((PX4_ISFINITE(_pos_sp_triplet.current.cruising_speed) && (_pos_sp_triplet.current.cruising_speed > 0.1f)) ?
+	return ((PX4_ISFINITE(_pos_sp_triplet.current.cruising_speed) && !(_pos_sp_triplet.current.cruising_speed < 0.0f)) ?
 		_pos_sp_triplet.current.cruising_speed : _params.vel_cruise_xy);
 }
 
@@ -2218,6 +2219,25 @@ void MulticopterPositionControl::control_auto(float dt)
 			}
 
 			_pos_sp = pos_sp;
+
+		} else if (_pos_sp_triplet.current.type == position_setpoint_s::SETPOINT_TYPE_VELOCITY) {
+
+			float vel_xy_mag = sqrtf(_vel(0) * _vel(0) + _vel(1) * _vel(1));
+
+			if (vel_xy_mag > SIGMA_NORM) {
+
+				_vel_sp(0) = _vel(0) / vel_xy_mag * get_cruising_speed_xy();
+				_vel_sp(1) = _vel(1) / vel_xy_mag * get_cruising_speed_xy();
+
+			} else {
+				/* TODO: we should go in the direction we are heading
+				 * if current velocity is zero
+				 */
+				_vel_sp(0) = 0.0f;
+				_vel_sp(1) = 0.0f;
+			}
+
+			_run_pos_control = false;
 
 		} else {
 			/* just go to the target point */;
